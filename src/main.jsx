4f12fcec31066512cc77e1e50fe0e0088d79513c
Rahul Chauhan <chauhansid06@gmail.com>
2023-11-30 07:23:46 +0530

    hero-section

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'react-datepicker/dist/react-datepicker.css';
 import './index.css';
+import Hero from './Hero.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hero />
   </React.StrictMode>,
 );
