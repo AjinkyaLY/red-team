61e1dac6e439d2933e287a1ba7d609c1cdfeea04
AjinkyaLY <ajinkya.b@ly.design>
2023-12-04 13:25:07 +0530

    scroll trigger added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Hero from './Hero';
 import Space from './Space';
+import Background from './assets/background.svg';
 
 function App() {
   return (
-    <div className='flex flex-col overflow-y-hidden'>
-      {/* <Hero /> */}
-      <div className='bg-[blue] w-[100vw] h-[100vh]'></div>
+    <div className='flex flex-col overflow-y-hidden h-full'>
+      <Hero />
       <Space />
+      <div id='div2' className='h-[500px]'>
+        <img src={Background} alt='' className='w-full h-full absolute object-cover z-[-1]' />
+      </div>
     </div>
   );
 }
--- src/Space.jsx
+++ src/Space.jsx
@@ -11,102 +11,308 @@ import { useEffect, useRef } from 'react';
 gsap.registerPlugin(ScrollTrigger);
 
 function Space() {
-  const spaceshipDivRef = useRef();
   const spaceshipRef = useRef();
+  const spaceshipTrigger = useRef();
+  const backdropRef = useRef();
+  const passageRef = useRef();
+  const passageTextRef = useRef();
+  const portalRef = useRef();
 
-  //   function spaceShipFun() {
-  //     gsap.set(spaceshipRef, { opacity: 0, scale: 0.2 });
-
-  //     gsap.fromTo(
-  //       spaceshipRef,
-  //       {
-  //         y: -1000,
-  //         x: 1500,
-  //         opacity: 1,
-  //         scale: 0.5,
-  //         display: 'flex',
-  //       },
-  //       {
-  //         opacity: 1,
-  //         display: 'flex',
-  //         duration: 2,
-  //         y: 0,
-  //         x: 0,
-  //         scale: 1.5,
-  //         ease: 'expoScale(0.5,7,none)',
-  //       },
-  //     );
-  //   }
-
-  function something() {
-    gsap.fromTo(
-      spaceshipRef.current,
-      {
-        y: -1000,
-        x: 1500,
-        opacity: 0,
-        scale: 0.2,
-        display: 'flex',
+  function spaceshipEnter() {
+    ScrollTrigger.create({
+      trigger: spaceshipTrigger.current,
+      start: 'top center',
+      end: 'center center',
+      toggleActions: 'play pause reverse pause',
+      onEnter: () => {
+        gsap.to(spaceshipTrigger.current, {
+          position: 'fixed',
+          top: 0,
+        });
+        gsap.fromTo(
+          spaceshipRef.current,
+          {
+            y: -1000,
+            x: 1500,
+            opacity: 0,
+            scale: 0.2,
+            display: 'flex',
+          },
+          {
+            opacity: 1,
+            display: 'flex',
+            duration: 1,
+            y: 0,
+            x: 0,
+            scale: 1.2,
+            ease: 'expoScale(0.5,7,none)',
+          },
+        );
       },
-      {
-        opacity: 1,
-        display: 'flex',
-        duration: 2,
-        y: 0,
-        x: 0,
-        scale: 1.2,
-        ease: 'expoScale(0.5,7,none)',
-        // scrollTrigger: {
-        //   trigger: spaceshipDivRef.current,
-        //   start: 'top center',
-        //   end: 'center center',
-        //   toggleActions: 'play none none reverse',
-        //   markers: true,
-        // },
+      onLeaveBack: () => {
+        gsap.to(spaceshipRef.current, {
+          opacity: 0,
+          display: 'none',
+          duration: 1,
+          y: -1000,
+          x: 1500,
+          scale: 0.2,
+          ease: 'expoScale(0.5,7,none)',
+        });
+        gsap.to(spaceshipTrigger.current, {
+          position: 'relative',
+          top: 0,
+        });
       },
-    );
+      // markers: true, // Remove this in production
+    });
+  }
+
+  function spaceshipZoom() {
+    ScrollTrigger.create({
+      trigger: spaceshipTrigger.current,
+      start: 'center center',
+      end: 'end center',
+      toggleActions: 'play pause reverse pause',
+      onEnter: () => {
+        gsap.to(spaceshipRef.current, {
+          duration: 1,
+          scale: 2,
+          ease: 'expoScale(0.5,7,none)',
+        });
+
+        gsap.to(backdropRef.current, {
+          opacity: 1,
+          duration: 1,
+          ease: 'expoScale(0.5,7,none)',
+          display: 'flex',
+        });
+        gsap.to(spaceshipRef.current, {
+          opacity: 1,
+          display: 'flex',
+          duration: 1,
+          ease: 'expoScale(0.5,7,none)',
+        });
+        gsap.to(passageRef.current, {
+          opacity: 0,
+          scale: 0,
+          display: 'none',
+          duration: 0.2,
+          ease: 'expoScale(0.5,7,none)',
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to(spaceshipRef.current, {
+          duration: 1,
+          scale: 1.2,
+          ease: 'expoScale(0.5,7,none)',
+          delay: 1,
+        });
+        gsap.to(backdropRef.current, {
+          opacity: 0,
+          duration: 1,
+          ease: 'expoScale(0.5,7,none)',
+          display: 'hidden',
+          delay: 1,
+        });
+        gsap.to(spaceshipRef.current, {
+          opacity: 1,
+          display: 'flex',
+          duration: 1,
+          ease: 'expoScale(0.5,7,none)',
+        });
+        gsap.to(passageRef.current, {
+          opacity: 0,
+          scale: 0,
+          display: 'none',
+          duration: 0.2,
+          ease: 'expoScale(0.5,7,none)',
+        });
+      },
+      // markers: true, // Remove this in production
+    });
+  }
+
+  function spaceshipPassage() {
+    ScrollTrigger.create({
+      trigger: spaceshipTrigger.current,
+      start: 'center 52%',
+      end: 'end 52%',
+      toggleActions: 'play pause reverse pause',
+      onEnter: () => {
+        gsap.to(spaceshipRef.current, {
+          opacity: 0,
+          display: 'none',
+          duration: 1,
+          ease: 'expoScale(0.5,7,none)',
+          delay: 1,
+        });
+
+        gsap.to(passageRef.current, {
+          opacity: 1,
+          scale: 1,
+          display: 'flex',
+          duration: 1,
+          ease: 'expoScale(0.5,7,none)',
+          delay: 1,
+        });
+        gsap.fromTo(
+          passageTextRef.current,
+          {
+            y: 500,
+            opacity: 0,
+          },
+          {
+            y: -150,
+            opacity: 1,
+            duration: 1,
+            ease: 'expoScale(0.5,7,none)',
+            display: 'flex',
+            delay: 2,
+          },
+        );
+      },
+      onLeaveBack: () => {
+        gsap.to(spaceshipRef.current, {
+          opacity: 1,
+          display: 'flex',
+          duration: 1,
+          ease: 'expoScale(0.5,7,none)',
+        });
+        gsap.to(passageRef.current, {
+          opacity: 0,
+          scale: 0,
+          display: 'none',
+          duration: 0.2,
+          ease: 'expoScale(0.5,7,none)',
+        });
+        gsap.fromTo(
+          passageTextRef.current,
+          {
+            y: -150,
+            opacity: 1,
+            display: 'flex',
+          },
+          {
+            y: 0,
+            opacity: 0,
+            duration: 0.5,
+            ease: 'expoScale(0.5,7,none)',
+            display: 'none',
+          },
+        );
+      },
+      //  markers: true, // Remove this in production
+    });
+  }
+
+  function toJersey() {
+    ScrollTrigger.create({
+      trigger: spaceshipTrigger.current,
+      start: 'center 10%',
+      end: 'end 10%',
+      toggleActions: 'play pause reverse pause',
+      onEnter: () => {
+        gsap.fromTo(
+          passageTextRef.current,
+          {
+            y: -150,
+            opacity: 1,
+          },
+          {
+            y: -800,
+            opacity: 0,
+            duration: 1.5,
+            ease: 'expoScale(0.5,7,none)',
+            display: 'none',
+          },
+        );
+        gsap.to(portalRef.current, {
+          opacity: 1,
+          rotateZ: 100,
+          duration: 2,
+          scale: 5,
+          ease: 'expoScale(0.5,7,none)',
+          display: 'flex',
+        });
+      },
+      onLeaveBack: () => {
+        gsap.fromTo(
+          passageTextRef.current,
+          {
+            y: -800,
+            opacity: 0,
+          },
+          {
+            y: -150,
+            opacity: 1,
+            duration: 1,
+            ease: 'expoScale(0.5,7,none)',
+            display: 'flex',
+          },
+        );
+        gsap.to(portalRef.current, {
+          opacity: 0,
+          rotateZ: 0,
+          duration: 2,
+          scale: 0,
+          ease: 'expoScale(0.5,7,none)',
+          display: 'none',
+        });
+      },
+      markers: true, // Remove this in production
+    });
   }
 
   useEffect(() => {
-    ScrollTrigger.create({
-      trigger: spaceshipDivRef.current,
-      start: 'top center',
-      end: 'center center',
-      toggleActions: 'restart pause reverse pause',
-      markers: true,
-      onEnter: something(),
-    });
+    spaceshipEnter();
+    spaceshipZoom();
+    spaceshipPassage();
+    toJersey();
   }, []);
 
   return (
     <div className='bg-black w-full h-full min-h-screen flex items-center'>
       <img src={Background} alt='' className='w-full h-full absolute object-cover z-0' />
-      <div className='relative flex justify-end items-center flex-col my-auto h-full min-h-screen max-h-screen w-full'>
-        <div ref={spaceshipDivRef} className='w-full h-full'>
-          <img
-            ref={spaceshipRef}
-            id='spaceship'
-            src={Spaceship}
-            alt=''
-            className='w-full h-full z-10'
-          />
-        </div>
-
-        {/* <div>
-          <img id='backdrop' src={Backdrop} alt='' className='w-full h-full object-cover z-20' />
-        </div>
-
-        <div>
-          <img id='passage' src={Passage} alt='' className='w-full h-full object-cover z-10' />
-        </div>
-
-        <div>
-          <img id='passageText' src={PassageText} alt='' className='w-[75%] h-[75%] z-40' />
-        </div>
-
-        <div>
-          <img id='portal' src={Portal} alt='' className='w-[75%] h-[75%] z-40 top-[120px]' />
-        </div> */}
+      <div
+        ref={spaceshipTrigger}
+        className='relative flex justify-end items-center flex-col my-auto h-full min-h-screen max-h-screen w-full'
+      >
+        <img
+          id='spaceship'
+          ref={spaceshipRef}
+          src={Spaceship}
+          alt=''
+          className='relative z-10 opacity-0 scale-[0.2] x-[1500]'
+        />
+        <img
+          id='backdrop'
+          ref={backdropRef}
+          src={Backdrop}
+          alt=''
+          className='w-full h-full absolute object-cover hidden z-20'
+        />
+        <img
+          id='passage'
+          ref={passageRef}
+          src={Passage}
+          alt=''
+          className='w-full h-full absolute object-cover hidden z-10'
+        />
+        <img
+          id='passageText'
+          ref={passageTextRef}
+          src={PassageText}
+          alt=''
+          className='w-[75%] h-[75%] absolute hidden z-40'
+        />
+        <img
+          id='portal'
+          ref={portalRef}
+          src={Portal}
+          alt=''
+          className='w-[75%] h-[75%] hidden absolute z-40 top-[120px] scale-0'
+        />
       </div>
     </div>
   );
