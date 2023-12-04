915309fe46bc8957f746df3122aaef77b0c1c460
AjinkyaLY <ajinkya.b@ly.design>
2023-12-04 16:44:51 +0530

    animation changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,59 +9,14 @@ import gsap from 'gsap';
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  const sliderRef = useRef();
-
-  function spaceshipEnter() {
-    const portal = [
-      document.getElementById('portal'),
-      document.getElementById('passage'),
-      document.getElementById('backdrop'),
-    ];
-    ScrollTrigger.create({
-      trigger: sliderRef.current,
-      start: 'top center',
-      end: 'center center',
-      toggleActions: 'play pause reverse pause',
-      onEnter: () => {
-        gsap.to(sliderRef.current, {
-          opacity: 1,
-          duration: 1,
-          ease: 'expoScale(0.5,7,none)',
-        });
-        gsap.to(portal, {
-          opacity: 0,
-          duration: 0.2,
-          ease: 'expoScale(0.5,7,none)',
-        });
-      },
-      onLeaveBack: () => {
-        gsap.to(sliderRef.current, {
-          opacity: 0,
-          duration: 1,
-          ease: 'expoScale(0.5,7,none)',
-        });
-        gsap.to(portal, {
-          opacity: 1,
-          duration: 0.2,
-          ease: 'expoScale(0.5,7,none)',
-        });
-      },
-      // markers: true, // Remove this in production
-    });
-  }
-
-  useEffect(() => {
-    spaceshipEnter();
-  }, []);
-
   return (
     <div className='flex flex-col overflow-y-hidden h-full'>
       <Hero />
       <Space />
-      <div className='h-[100px]'>
+      <div className='h-[50px]'>
         <img src={Background} alt='' className='w-full h-full absolute object-cover z-[-1]' />
       </div>
-      <div ref={sliderRef} className='opacity-0'>
+      <div id='sliderRef' className='opacity-0'>
         <Slider />
       </div>
     </div>
