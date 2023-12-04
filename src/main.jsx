68895e8b2332a2839c524aed50153d2e6be5917f
AjinkyaLY <ajinkya.b@ly.design>
2023-12-04 10:29:50 +0530

    chanegs

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'react-datepicker/dist/react-datepicker.css';
 import './index.css';
-import Hero from './Hero.jsx';
-import Space from './Space.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Hero />
-    <Space />
+    <App />
   </React.StrictMode>,
 );
